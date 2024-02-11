0b0da9f1d4d2e215ea4a9c25340699fc8f35c301
edwin harley martinez gomez <alpha0202@gmail.com>
2024-02-11 11:42:21 -0500

    Agregar archivos de proyecto.

---
+++ NetFirebase.Api/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,24 @@
+﻿
+using FirebaseAdmin.Auth;
+
+namespace NetFirebase.Api.Services.Authentication
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        public async Task<string> RegisterAsync(string email, string password)
+        {
+            var userArgs = new UserRecordArgs
+            {
+                Email = email,
+                Password = password
+            };
+
+            var usuario = await FirebaseAuth.DefaultInstance.CreateUserAsync(userArgs);
+
+            return usuario.Uid;
+        }
+
+
+
+    }
+}
---
+++ NetFirebase.Api/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,7 @@
+﻿namespace NetFirebase.Api.Services.Authentication
+{
+    public interface IAuthenticationService
+    {
+        Task<string> RegisterAsync(string email, string password);
+    }
+}
